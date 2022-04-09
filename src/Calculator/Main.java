05b48014460905cd9fe984ae3a8bd0c96668134a
Kalamadeo93 <persoonaj@gmail.com>
2022-04-09 23:26:15 +0300

    .

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -15,19 +15,73 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Main main = new Main();
-        int[] arr = {3,5,20,8,7,3,100};
-        System.out.println(Arrays.toString(printOddNumbers(arr)));
+        int[] arr = {-7,-5,-2,0,1,5,9};
+        int[] arr1 = {-4,-1,2,4,8};
+        System.out.println(Arrays.toString(mergeArrays(arr, arr1)));
     }
 
-    public static String[] printOddNumbers(int[] arr) {
-        String[] copy = new String[arr.length];
-        for (int i = 0,j = 0;i < arr.length; i++) {
-            if (arr[i] % 2 != 0) {
-                copy[j] = Arrays.toString(new int[]{arr[i]});
+    public static int[] mergeArrays(int[] a1, int[] a2) {
+        int a = a1.length;
+        int b = a2.length;
+        int c = a + b;
+        int[] copy = new int[c];
+        int i = 0,j = 0,k = 0;
+        while (i < a && j < b) {
+            int n = a1[i];
+            int m = a2[j];
+            if (n > m) {
+                copy[k] = m;
                 j++;
+                k++;
+            } else if (m > n) {
+                copy[k] = n;
+                i++;
+                k++;
             }
+        }
+        while(i < a) {
+            copy[k] = a1[i];
+            k++;
+            i++;
+        }
+        while(j < b) {
+            copy[k] = a2[j];
+            k++;
+            j++;
         }
         return copy;
     }
 
 }
+/*if (a != 0){
+                if (a > b) {
+                    while (i < a && j < b) {
+                        int n = arr[i]; // n = arr[0] = 0
+                        int m = arr1[j]; // m = arr1[0] = 6
+                        if (n > m) {// 0 > 6 // not true
+                            copy[k] = m;
+                            j++;
+                            count++;
+                        } else if (m > n) { // 6 > 0 //true
+                            copy[k] = n;
+                            i++;
+                            count++;// copy[0] = 0
+                        }
+                        k++;
+                    }
+                    while(i < a) {
+                        copy[k] = arr[i];
+                        k++;
+                        i++;
+                        count++;
+                    }
+                    while(j < b) {
+                        copy[k] = arr[j];
+                        k++;
+                        j++;
+                        count++;
+                    }
+                }
+            } else if (b != 0) {
+
+            }*/
